fa2e9330bdb9a130b45148966ba5e511b78e527e
Parixshit <87859520+Parixshit@users.noreply.github.com>
2023-06-12 17:22:37 +0530

    update repos

--- sync.sh
+++ sync.sh
@@ -1,19 +1,32 @@
 # Remove
-rm -rf vendor/qcom/opensource/commonsys/display
-rm -rf vendor/qcom/opensource/commonsys-intf/display
+rm -rf device/qcom/common
 
 # Device
 git clone -b tiramisu https://github.com/parixshit-ci/device_xiaomi_spes.git device/xiaomi/spes
+git clone -b thirteen https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_spes.git vendor/xiaomi/spes
+git clone -b thirteen https://github.com/PixelExperience-Devices/device_xiaomi_spes-kernel.git device/xiaomi/spes-kernel
+git clone -b thirteen https://github.com/PixelExperience-Devices/kernel_xiaomi_sm6225.git kernel/xiaomi/sm6225
 git clone -b tiramisu https://github.com/parixshit-ci/device_xiaomi_sm6225-common.git device/xiaomi/sm6225-common
-git clone -b thirteen https://github.com/aospa/android_device_xiaomi_spes-kernel.git device/xiaomi/spes-kernel
-git clone -b tiramisu https://github.com/parixshit-ci/proprietary_vendor_xiaomi.git vendor/xiaomi
+git clone -b thirteen https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_sm6225-common.git vendor/xiaomi/sm6225-common
 
 # Hardware
-git clone -b thirteen https://github.com/aospa/android_hardware_xiaomi.git hardware/xiaomi
-git clone -b thirteen https://github.com/aospa/android_hardware_qcom_gps.git hardware/qcom/gps
-git clone -b thirteen https://github.com/aospa/android_hardware_qcom_media.git hardware/qcom/media
-git clone -b thirteen https://github.com/aospa/android_hardware_qcom_display.git hardware/qcom/display
+git clone -b thirteen https://github.com/PixelExperience/hardware_xiaomi.git hardware/xiaomi
+git clone -b thirteen https://github.com/PixelExperience/hardware_st_nfc.git hardware/st/nfc
 
-# Vendor
-git clone -b thirteen https://github.com/aospa/android_vendor_qcom-opensource_display-commonsys.git vendor/qcom/opensource/commonsys/display
-git clone -b thirteen https://github.com/aospa/android_vendor_qcom_opensource_commonsys-intf_display.git vendor/qcom/opensource/commonsys-intf/display
+# Device (QCOM)
+git clone -b thirteen https://github.com/PixelExperience-Devices/device_qcom_common.git device/qcom/common
+git clone -b thirteen https://github.com/PixelExperience-Devices/device_qcom_wlan.git device/qcom/wlan
+git clone -b thirteen https://github.com/PixelExperience-Devices/device_qcom_common-sepolicy.git device/qcom/common-sepolicy
+git clone -b thirteen https://github.com/PixelExperience-Devices/device_qcom_qssi.git device/qcom/qssi
+git clone -b thirteen https://github.com/PixelExperience-Devices/device_qcom_vendor-common.git device/qcom/vendor-common
+
+# Vendor (QCOM)
+git clone -b thirteen https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_qcom_common.git vendor/qcom/common
+git clone -b thirteen https://github.com/PixelExperience/vendor_qcom_opensource_core-utils.git vendor/qcom/opensource/core-utils
+git clone -b thirteen https://github.com/PixelExperience/vendor_qcom_opensource_fm-commonsys.git vendor/qcom/opensource/fm-commonsys
+git clone -b thirteen https://github.com/PixelExperience/vendor_qcom_opensource_interfaces.git vendor/qcom/opensource/interfaces
+git clone -b thirteen https://github.com/PixelExperience/vendor_qcom_opensource_commonsys-intf_bluetooth.git vendor/qcom/opensource/commonsys-intf/bluetooth
+
+. build/envsetup.sh
+lunch evolution_spes-user
+mka evolution
