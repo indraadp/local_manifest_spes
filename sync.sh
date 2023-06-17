ee5a74381f718e7feb25e3582473393a266d84a7
Parixshit <87859520+Parixshit@users.noreply.github.com>
2023-06-17 11:33:38 +0530

    spes: derp
    Signed-off-by: Parixshit <87859520+Parixshit@users.noreply.github.com>

--- sync.sh
+++ sync.sh
@@ -1,17 +1,11 @@
 # Remove
-rm -rf device/qcom/common
 rm -rf hardware/st/nfc
-# rm -rf hardware/xiaomi
-# rm -rf hardware/qcom-caf/bengal/gps
-# rm -rf hardware/qcom-caf/bengal/media
-# rm -rf hardware/qcom-caf/bengal/audio
-# rm -rf hardware/qcom-caf/bengal/display
 rm -rf vendor/qcom/opensource/interfaces
 rm -rf vendor/qcom/opensource/fm-commonsys
 
 # Device
-git clone --depth=1 -b tiramisu https://github.com/parixshit-ci/device_xiaomi_spes.git device/xiaomi/spes
-git clone --depth=1 -b tiramisu https://github.com/parixshit-ci/device_xiaomi_sm6225-common.git device/xiaomi/sm6225-common
+git clone --depth=1 -b 13 https://github.com/parixshit-ci/device_xiaomi_spes.git device/xiaomi/spes
+git clone --depth=1 -b 13 https://github.com/parixshit-ci/device_xiaomi_sm6225-common.git device/xiaomi/sm6225-common
 
 git clone --depth=1 -b thirteen https://github.com/PixelExperience-Devices/kernel_xiaomi_sm6225.git kernel/xiaomi/sm6225
 git clone --depth=1 -b thirteen https://github.com/PixelExperience-Devices/device_xiaomi_spes-kernel.git device/xiaomi/spes-kernel
@@ -21,17 +15,17 @@ git clone --depth=1 -b thirteen https://gitlab.pixelexperience.org/android/vendo
 
 # Hardware
 git clone --depth=1 -b thirteen https://github.com/PixelExperience/hardware_st_nfc.git hardware/st/nfc
-# git clone --depth=1 -b thirteen https://github.com/PixelExperience/hardware_xiaomi.git hardware/xiaomi
-# git clone --depth=1 -b thirteen https://github.com/PixelExperience/hardware_qcom-caf_bengal_gps.git hardware/qcom-caf/bengal/gps
-# git clone --depth=1 -b thirteen https://github.com/PixelExperience/hardware_qcom-caf_bengal_media.git hardware/qcom-caf/bengal/media
-# git clone --depth=1 -b thirteen https://github.com/PixelExperience/hardware_qcom-caf_bengal_audio.git hardware/qcom-caf/bengal/audio
-# git clone --depth=1 -b thirteen https://github.com/PixelExperience/hardware_qcom-caf_bengal_display.git hardware/qcom-caf/bengal/display
+git clone --depth=1 -b thirteen https://github.com/PixelExperience/hardware_xiaomi.git hardware/xiaomi
+git clone --depth=1 -b thirteen https://github.com/PixelExperience/hardware_qcom-caf_bengal_gps.git hardware/qcom-caf/bengal/gps
+git clone --depth=1 -b thirteen https://github.com/PixelExperience/hardware_qcom-caf_bengal_media.git hardware/qcom-caf/bengal/media
+git clone --depth=1 -b thirteen https://github.com/PixelExperience/hardware_qcom-caf_bengal_audio.git hardware/qcom-caf/bengal/audio
+git clone --depth=1 -b thirteen https://github.com/PixelExperience/hardware_qcom-caf_bengal_display.git hardware/qcom-caf/bengal/display
 
 # Device (QCOM)
-git clone --depth=1 -b tiramisu https://github.com/parixshit-ci/device_qcom_common.git device/qcom/common
+git clone --depth=1 -b 13 https://github.com/parixshit-ci/device_qcom_common.git device/qcom/common
 git clone --depth=1 -b thirteen https://github.com/PixelExperience-Devices/device_qcom_qssi.git device/qcom/qssi
 git clone --depth=1 -b thirteen https://github.com/PixelExperience-Devices/device_qcom_wlan.git device/qcom/wlan
-git clone --depth=1 -b tiramisu https://github.com/parixshit-ci/device_qcom_common-sepolicy.git device/qcom/common-sepolicy
+git clone --depth=1 -b 13 https://github.com/parixshit-ci/device_qcom_common-sepolicy.git device/qcom/common-sepolicy
 git clone --depth=1 -b thirteen https://github.com/PixelExperience-Devices/device_qcom_vendor-common.git device/qcom/vendor-common
 
 # Vendor (QCOM)
@@ -43,8 +37,8 @@ git clone --depth=1 -b thirteen https://github.com/PixelExperience/vendor_qcom_o
 git clone --depth=1 -b thirteen https://github.com/PixelExperience/vendor_qcom_opensource_commonsys-intf_bluetooth.git vendor/qcom/opensource/commonsys-intf/bluetooth
 
 # . build/envsetup.sh
-# lunch evolution_spes-user
-# mka evolution
+# lunch derp_spes-user
+# mka derp
 
 # cd .. && chmod 600 ssh-key
 # scp -i ssh-key evoX/out/target/product/spes/evolution_spes-ota* parixshit@frs.sourceforge.net:/home/frs/project/parixshit/Evolution
