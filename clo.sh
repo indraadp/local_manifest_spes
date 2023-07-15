42c709a432615608b201df17c9dfc3c492bb12d9
Parixshit <87859520+Parixshit@users.noreply.github.com>
2023-07-15 10:18:12 +0530

    lineage clo.sh

---
+++ clo.sh
@@ -0,0 +1,46 @@
+# Remove
+rm -rf hardware/st/nfc
+
+# Device
+git clone -b lineage-20.0 https://github.com/parixxshit/device_xiaomi_spes.git device/xiaomi/spes
+git clone -b lineage-20.0 https://github.com/parixxshit/device_xiaomi_sm6225-common.git device/xiaomi/sm6225-common
+
+git clone -b thirteen https://github.com/PixelExperience-Devices/kernel_xiaomi_sm6225.git kernel/xiaomi/sm6225
+git clone -b thirteen https://github.com/PixelExperience-Devices/device_xiaomi_spes-kernel.git device/xiaomi/spes-kernel
+
+git clone -b thirteen https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_spes.git vendor/xiaomi/spes
+git clone -b thirteen https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_sm6225-common.git vendor/xiaomi/sm6225-common
+
+# Packages
+
+# Hardware
+git clone -b thirteen https://github.com/PixelExperience/hardware_st_nfc.git hardware/st/nfc
+git clone -b thirteen https://github.com/PixelExperience/hardware_xiaomi.git hardware/xiaomi
+
+git clone -b thirteen https://github.com/PixelExperience/hardware_qcom-caf_bengal_gps.git hardware/qcom-caf/bengal/gps
+git clone -b thirteen https://github.com/PixelExperience/hardware_qcom-caf_bengal_media.git hardware/qcom-caf/bengal/media
+git clone -b thirteen https://github.com/PixelExperience/hardware_qcom-caf_bengal_audio.git hardware/qcom-caf/bengal/audio
+git clone -b thirteen https://github.com/PixelExperience/hardware_qcom-caf_bengal_display.git hardware/qcom-caf/bengal/display
+
+# Device (QCOM)
+git clone -b thirteen https://github.com/PixelExperience-Devices/device_qcom_qssi.git device/qcom/qssi
+git clone -b thirteen https://github.com/PixelExperience-Devices/device_qcom_wlan.git device/qcom/wlan
+git clone -b thirteen https://github.com/PixelExperience-Devices/device_qcom_common.git device/qcom/common
+git clone -b thirteen https://github.com/PixelExperience-Devices/device_qcom_vendor-common.git device/qcom/vendor-common
+git clone -b thirteen https://github.com/PixelExperience-Devices/device_qcom_common-sepolicy.git device/qcom/common-sepolicy
+
+# Vendor (QCOM)
+git clone -b thirteen https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_qcom_common.git vendor/qcom/common
+git clone -b thirteen https://github.com/PixelExperience/vendor_qcom_opensource_core-utils.git vendor/qcom/opensource/core-utils
+git clone -b thirteen https://github.com/PixelExperience/vendor_qcom_opensource_interfaces.git vendor/qcom/opensource/interfaces
+git clone -b thirteen https://github.com/PixelExperience/vendor_qcom_opensource_commonsys_dpm.git vendor/qcom/opensource/commonsys/dpm
+git clone -b thirteen https://github.com/PixelExperience/vendor_qcom_opensource_commonsys-intf_bluetooth.git vendor/qcom/opensource/commonsys-intf/bluetooth
+
+# Patch
+cd vendor/derp
+git fetch https://github.com/parixxshit/vendor_derp.git
+git cherry-pick 82a4247dfd884cec0a177fb1b4c9407da7abc863 4edc8123ba3dc98f515f25a17ed3d97af39f6f22
+cd ../..
+
+. build/envsetup.sh
+# lunch lineage_spes-userdebug && m bacon
