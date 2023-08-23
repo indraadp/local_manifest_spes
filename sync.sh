d72ae4d72b72ca6f698c3b51f3a88f44324b8fff
Parixshit <87859520+Parixshit@users.noreply.github.com>
2023-08-23 12:20:03 +0530

    Update sync.sh
    Signed-off-by: Parixshit <87859520+Parixshit@users.noreply.github.com>

--- sync.sh
+++ sync.sh
@@ -3,26 +3,28 @@ rm -rf hardware/st/nfc
 rm -rf packages/apps/Dialer
 rm -rf packages/apps/Contacts
 rm -rf packages/apps/Messaging
-# rm -rf packages/apps/ExactCalculator
 rm -rf packages/resources/devicesettings
 
 # Device
 git clone -b 13 https://github.com/parixxshit/device_xiaomi_spes.git device/xiaomi/spes
 git clone -b 13 https://github.com/parixxshit/device_xiaomi_sm6225-common.git device/xiaomi/sm6225-common
-git clone -b thirteen https://github.com/PixelExperience-Devices/device_xiaomi_sm6225-common-miuicamera.git device/xiaomi/sm6225-common-miuicamera
 
-git clone -b thirteen https://github.com/PixelExperience-Devices/kernel_xiaomi_sm6225.git kernel/xiaomi/sm6225
-git clone -b thirteen https://github.com/PixelExperience-Devices/device_xiaomi_spes-kernel.git device/xiaomi/spes-kernel
+# Kernel
+git clone -b thirteen --depth=1 https://github.com/PixelExperience-Devices/kernel_xiaomi_sm6225.git kernel/xiaomi/sm6225
+git clone -b thirteen --depth=1 https://github.com/PixelExperience-Devices/device_xiaomi_spes-kernel.git device/xiaomi/spes-kernel
 
-git clone -b thirteen https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_spes.git vendor/xiaomi/spes
-git clone -b thirteen https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_sm6225-common.git vendor/xiaomi/sm6225-common
-git clone -b thirteen https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_sm6225-common-miuicamera.git vendor/xiaomi/sm6225-common-miuicamera
+# Vendor
+git clone -b thirteen --depth=1 https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_spes.git vendor/xiaomi/spes
+git clone -b thirteen --depth=1 https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_sm6225-common.git vendor/xiaomi/sm6225-common
+
+# MIUI Camera
+git clone -b thirteen --depth=1 https://github.com/PixelExperience-Devices/device_xiaomi_sm6225-common-miuicamera.git device/xiaomi/sm6225-common-miuicamera
+git clone -b thirteen --depth=1 https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_sm6225-common-miuicamera.git vendor/xiaomi/sm6225-common-miuicamera
 
 # Packages
 git clone -b tokui https://github.com/P-404/android_packages_apps_Dialer.git packages/apps/Dialer
 git clone -b tokui https://github.com/P-404/android_packages_apps_Contacts.git packages/apps/Contacts
 git clone -b tokui https://github.com/P-404/android_packages_apps_Messaging.git packages/apps/Messaging
-# git clone -b tokui https://github.com/P-404/android_packages_apps_ExactCalculator.git packages/apps/ExactCalculator
 git clone -b thirteen https://github.com/PixelExperience/packages_resources_devicesettings.git packages/resources/devicesettings
 
 # Hardware
@@ -42,10 +44,10 @@ git clone -b thirteen https://github.com/PixelExperience-Devices/device_qcom_com
 git clone -b thirteen https://github.com/PixelExperience-Devices/device_qcom_vendor-common.git device/qcom/vendor-common
 
 # Vendor (QCOM)
-git clone -b thirteen https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_qcom_common.git vendor/qcom/common
-git clone -b thirteen https://github.com/PixelExperience/vendor_qcom_opensource_core-utils.git vendor/qcom/opensource/core-utils
-git clone -b thirteen https://github.com/PixelExperience/vendor_qcom_opensource_commonsys_dpm.git vendor/qcom/opensource/commonsys/dpm
-git clone -b thirteen https://github.com/PixelExperience/vendor_qcom_opensource_commonsys-intf_bluetooth.git vendor/qcom/opensource/commonsys-intf/bluetooth
+git clone -b thirteen --depth=1 https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_qcom_common.git vendor/qcom/common
+git clone -b thirteen --depth=1 https://github.com/PixelExperience/vendor_qcom_opensource_core-utils.git vendor/qcom/opensource/core-utils
+git clone -b thirteen --depth=1 https://github.com/PixelExperience/vendor_qcom_opensource_commonsys_dpm.git vendor/qcom/opensource/commonsys/dpm
+git clone -b thirteen --depth=1 https://github.com/PixelExperience/vendor_qcom_opensource_commonsys-intf_bluetooth.git vendor/qcom/opensource/commonsys-intf/bluetooth
 
 # Patch frameworks/base
 pushd frameworks/base
@@ -68,7 +70,7 @@ popd
 # Patch device/qcom/common-sepolicy
 pushd device/qcom/common-sepolicy
 git fetch https://github.com/parixxshit/device_qcom_common-sepolicy.git
-git cherry-pick 1a7aa02 # Adapt sepolicy for DerpFest
+git cherry-pick e4c9045 # Adapt sepolicy for DerpFest
 popd
 
 # Patch packages/resources/devicesettings
@@ -84,17 +86,15 @@ popd
 # popd
 
 # Patch packages/apps/Updater
-pushd packages/apps/Updater
-git fetch https://github.com/parixxshit/packages_apps_Updater.git
-git cherry-pick 7554a70 # Update server and changelog url
-popd
+# pushd packages/apps/Updater
+# git fetch https://github.com/parixxshit/packages_apps_Updater.git
+# git cherry-pick 7554a70 # Update server and changelog url
+# popd
 
 # Patch vendor/derp
 pushd vendor/derp
-git fetch https://github.com/parixxshit/vendor_derp.git -t 13-custom
-git cherry-pick 95d7761^..cf1f878 # Last 5
-# Add: https://github.com/AOSPA/android_vendor_aospa/commit/79d30123868e4943b563598b0a20d3a1aaf33732
-# Here: https://github.com/DerpFest-AOSP/vendor_derp/blob/13/config/common.mk "Blur"
+git fetch https://github.com/parixxshit/vendor_derp.git
+git cherry-pick 01370e0 33d01f6 # Allow QCOM FM HAL & Split 4.19 SoC family
 popd
 
 # Patch vendor/qcom/opensource/fm-commonsys
